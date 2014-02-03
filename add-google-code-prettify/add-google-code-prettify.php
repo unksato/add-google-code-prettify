57b120719c3795ebb811497c054618f0d33d64f8
Satoshi Watanabe <unksato@gmail.com>
2014-02-03 15:11:41 +0900

    fix wp_admin graph issue

--- add-google-code-prettify/add-google-code-prettify.php
+++ add-google-code-prettify/add-google-code-prettify.php
@@ -8,12 +8,11 @@ Version: 0.1
 Author: Satoshi Watanabe
 Author URI: http://unk.jp.net/
 */
-// styles
-function add_google_code_prettify_header() {
-        $google_code_prettify_script = "\n<script src=\"https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js?lang=css&skin=sunburst\"></script>\n";
-        print($google_code_prettify_script);
+function add_google_code_script(){
+	$google_code_script = "<script src='https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js?lang=css&skin=sunburst'></script>";
+	echo $google_code_script;
 }
 
-add_action('wp_head', 'add_google_code_prettify_header');
-?>
+add_action('wp_head', 'add_google_code_script');
 
+?>
